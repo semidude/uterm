5fdb0e68809148d75e3db994373ebb5bc941e871
Radosław Panuszewski <radoslaw.panuszewski15@gmail.com>
2019-12-05 16:39:37 +0100

    Initialize CMake project

---
+++ cmake-build-debug/CMakeFiles/uterm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uterm.dir/main.cpp.o"
+  "uterm"
+  "uterm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uterm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
